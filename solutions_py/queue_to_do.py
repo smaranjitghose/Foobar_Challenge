c071902b9b0de7da58061f1539208854fe106e9e
Smaranjit Ghose <46641503+smaranjitghose@users.noreply.github.com>
2020-05-01 08:47:32 +0530

    Added Level3Q1

---
+++ solutions_py/queue_to_do.py
@@ -0,0 +1,14 @@
+def answer(start, length):
+
+    def bulk_xor(m, n):
+        m -= 1
+        f_m = [m, 1, m + 1, 0][m % 4]
+        f_n = [n, 1, n + 1, 0][n % 4]
+        return f_m ^ f_n
+
+    xor = 0
+    for i in range(length):
+        xor ^= bulk_xor(start, start + length - i - 1)
+        start += length
+
+    return xor
