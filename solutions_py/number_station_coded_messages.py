85dfa90657c3589b33ee40e6659d7a0133a2fa2b
Smaranjit Ghose <46641503+smaranjitghose@users.noreply.github.com>
2020-05-01 08:37:17 +0530

    Added Level2Q2

---
+++ solutions_py/number_station_coded_messages.py
@@ -0,0 +1,10 @@
+def answer(l, t):
+    length = len(l)
+    for start_index in range(length):
+        for end_index in range(start_index, length):
+            goal = sum(l[start_index:end_index+1])
+            if (goal > t):
+                break
+            if (goal == t):
+                return [start_index, end_index]
+    return [-1, -1]
