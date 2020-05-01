d667e853785f0bcfe5521d431d6ba07c6b272c63
Smaranjit Ghose <46641503+smaranjitghose@users.noreply.github.com>
2020-05-01 08:51:21 +0530

    Added Level3Q2

---
+++ solutions_py/the_grandest_staircase_of_them_all.py
@@ -0,0 +1,11 @@
+def answer(n):
+    st = [[0] * n for _ in range(n + 1)]
+    st[0][0] = st[1][1] = st[2][2] = 1
+
+    for j in range(1, n):
+        for i in range(n + 1):
+            st[i][j] = st[i][j - 1]
+            if i >= j:
+                st[i][j] += st[i - j][j - 1]
+
+    return st[-1][-1]
