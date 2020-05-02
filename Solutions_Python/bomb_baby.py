7e2a2cee9e59be48788b3e3c729c1c8864b555ce
Sukkrit <sukkritsharmaofficial@gmail.com>
2020-05-02 22:01:54 +0530

    Added bomb_baby.py

---
+++ Solutions_Python/bomb_baby.py
@@ -0,0 +1,11 @@
+def answer(x, y):
+    generations = 0
+    min_max = [int(x), int(y)]
+    while min_max[0] > 1 and min_max[1] > 1:
+        min_max.sort()
+        if min_max[1] %  min_max[0] == 0:
+            return "impossible"
+        generations += min_max[1] // min_max[0]
+        min_max[1]  = min_max[1] %  min_max[0]
+    generations += max(min_max) - 1
+    return str(generations)
