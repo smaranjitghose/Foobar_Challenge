82ef2a04fdb5dc35ba11696b27e0ddaf2702f77e
someshkoli <kolisomesh27@gmail.com>
2020-05-03 01:54:32 +0530

    Adds #Doomday fule - adds problem statement for doomsday fule

--- Solutions_Python/find_the_access_codes.py
+++ Solutions_Python/find_the_access_codes.py
@@ -1,8 +1,8 @@
 def solution(l):
-    factor = [[]] * len(l)
+    div = [[]] * len(l)
     for i in range(1, len(l)):
-        factor[i] = [j for j in range(i) if l[i] % l[j] == 0]
+        div[i] = [j for j in range(i) if l[i] % l[j] == 0]
     count = 0
     for i in range(2, len(l)):
-        count += sum(len(factor[j]) for j in factor[i])
+        count += sum(len(div[j]) for j in div[i])
     return count
